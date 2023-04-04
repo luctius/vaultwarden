74f1b3b51b21e59121b487a2cc8a142c2dbec455
Cor Peters <C.Peters@Eekels.com>
2023-04-04 08:22:34 +0200

    Addded custom scripts for building and uploading vaultwarden to my nas.

---
+++ update.sh
@@ -0,0 +1,3 @@
+git fetch
+git checkout main && git pull origin main
+git checkout personal_scripts && git pull --rebase origin main
---
+++ upload.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+cargo clean
+./update.sh
+mkdir -p ./target/armv7-unknown-linux-gnueabihf/release/deps/ &&  cp ./deps/* ./target/armv7-unknown-linux-gnueabihf/release/deps/
+cargo zigbuild --target armv7-unknown-linux-gnueabihf.2.26 --features vendored_openssl,sqlite --release  || cp ./deps/* ./target/armv7-unknown-linux-gnueabihf/release/build/ring-*/out/
+cargo zigbuild --target armv7-unknown-linux-gnueabihf.2.26 --features vendored_openssl,sqlite --release &&  scp target/armv7-unknown-linux-gnueabihf/release/vaultwarden nas:/home/bitwarden/bitwarden_rs.new && cargo clean
+#cross build --target armv7-unknown-linux-musleabihf --features vendored_openssl,sqlite --release 
+WEB_VAULT_FILE=$(curl -s https://api.github.com/repos/dani-garcia/bw_web_builds/releases/latest | grep "browser_download_url.*\.tar\.gz" | cut -d : -f 2,3 | tr -d \" | head -n 1)
+ssh nas "cd bitwarden && rm -rf web-vault.previous ; mv web-vault web-vault.previous ; ./bitwarden_rs -v ; echo ${WEB_VAULT_FILE} | wget -qi - && tar -zxf bw_web_v*.tar.gz && rm bw_web_v*.tar.gz"
