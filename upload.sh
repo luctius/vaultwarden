9719900cd367813d3240c8ce637196c973f61bb9
Cor Peters <C.Peters@Eekels.com>
2023-07-23 23:29:05 +0200

    added comment about downloading latest web-vault

--- upload.sh
+++ upload.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cargo clean 
 ./update.sh &&  cross build --target armv7-unknown-linux-musleabihf --features vendored_openssl,sqlite --release &&  scp target/armv7-unknown-linux-musleabihf/release/vaultwarden nas:bitwarden/bitwarden_rs.new && cargo clean
+echo "download https://github.com/dani-garcia/bw_web_builds/releases/latest"
