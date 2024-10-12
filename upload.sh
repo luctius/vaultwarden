386ce4ef3f4c1bd16a594dfc2cc0e6b2ac1d26fd
Cor Peters <C.Peters@Eekels.com>
2024-10-13 01:27:34 +0200

    use zigbuild

--- upload.sh
+++ upload.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
-cargo clean 
-./update.sh &&  cross build --target armv7-unknown-linux-musleabihf --features vendored_openssl,sqlite --release &&  scp target/armv7-unknown-linux-musleabihf/release/vaultwarden nas:/home/bitwarden/bitwarden_rs.new && cargo clean
+#!/bin/bash -x
+cargo clean
+mkdir -p ./vaultwarden/target/armv7-unknown-linux-gnueabihf/release/deps/ &&  cp ./deps/* ./vaultwarden/target/armv7-unknown-linux-gnueabihf/release/deps/
+./update.sh && cargo zigbuild --target armv7-unknown-linux-gnueabihf.2.26 --features vendored_openssl,sqlite --release &&  scp target/armv7-unknown-linux-musleabihf/release/vaultwarden nas:/home/bitwarden/bitwarden_rs.new && cargo clean
+#cross build --target armv7-unknown-linux-musleabihf --features vendored_openssl,sqlite --release 
 WEB_VAULT_FILE=$(curl -s https://api.github.com/repos/dani-garcia/bw_web_builds/releases/latest | grep "browser_download_url.*\.tar\.gz" | cut -d : -f 2,3 | tr -d \" | head -n 1)
 ssh nas "cd bitwarden && rm -rf web-vault.previous ; mv web-vault web-vault.previous ; echo ${WEB_VAULT_FILE} | wget -qi - && tar -zxv bw_web_v*.tar.gz && rm bw_web_v*.tar.gz"
