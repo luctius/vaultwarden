5ad7f2cf83f97539adead96a2d8f489f77337dd3
Cor Peters <C.Peters@Eekels.com>
2024-10-13 21:07:32 +0200

    wip

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 scp -r nas:bitwarden/data/db.sqlite3 data
-target/release/vaultwarden ||  cargo run --features vendored_openssl,sqlite --release
+./target/release/vaultwarden ||  cargo run --features vendored_openssl,sqlite --release
